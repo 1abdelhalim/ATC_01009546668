41dfe299924c404d8d2cef844d2591236c4f2e3a
abdelhalim <mohammadabdelhalim5@gmail.com>
2025-05-17 11:41:35 +0300

    Fix AUTH_USER_MODEL to point to CustomUser

--- event_booking/settings_postgres.py
+++ event_booking/settings_postgres.py
@@ -151,7 +151,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # User model
-AUTH_USER_MODEL = 'users.User'
+AUTH_USER_MODEL = 'users.CustomUser'
 
 # Security settings (enable for production)
 if not DEBUG:
